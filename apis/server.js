9f6e20cc1d32b379d8b22a46008e7b03d5cb0bcb
thinhnp <thinhnp26@fpt.com>
2025-04-29 18:37:25 +0700

    feat: add server

---
+++ apis/server.js
@@ -0,0 +1,38 @@
+import cors from "cors";
+import jsonServer from "json-server";
+import morgan from "morgan";
+import queryString from "query-string";
+
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+router.render = (req, res) => {
+  const headers = res.getHeaders();
+
+  const totalCountHeader = headers["x-total-count"];
+  if (req.method === "GET" && totalCountHeader) {
+    const queryParams = queryString.parse(req._parsedUrl.query);
+
+    const result = {
+      docs: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _total: Number.parseInt(totalCountHeader),
+      },
+    };
+
+    return res.jsonp(result);
+  }
+
+  res.jsonp(res.locals.data);
+};
+
+server.use(cors());
+server.use(morgan("dev"));
+server.use("/api", router);
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log("JSON Server is running at port", PORT);
+});
